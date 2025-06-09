2bf62ad21a487b4ef1c3542fb2407989d1de574d
OhItsLena <lena.hammerer@vol.at>
2025-06-09 17:24:31 +0200

    fix file name

--- startup.bat
+++ startup.bat
@@ -1,6 +1,6 @@
 @echo off
 cd C:\webapp
 call venv\Scripts\activate.bat
-start "" venv\Scripts\pythonw.exe runserver.py
+start "" venv\Scripts\pythonw.exe app.py
 timeout /t 3
 start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8085
