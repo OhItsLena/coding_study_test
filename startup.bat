261cda85f78af876c25ba13fb440922f6c5362a5
OhItsLena <lena.hammerer@vol.at>
2025-08-29 16:41:18 +0200

    remove unused functionality

--- startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\webapp
-git pull origin main --quiet
-call venv\Scripts\activate.bat
-start "" venv\Scripts\pythonw.exe app.py
-timeout /t 5
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:39765
